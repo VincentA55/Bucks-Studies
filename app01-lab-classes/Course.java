e3ec57a3be9a73ec4a9a89c51c4c23a0a98b4381
VincentA55 <vinyl272@gmail.com>
2020-09-30 10:09:58 +0100

    + txt files

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author (Vincent Assolutissimamente)
+ * @version (30/9/2020)
+ */
+public class Course
+{
+    // the course code
+    private String codeNumber;
+    // the course title
+    private String title;
+
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course(String courseCode, String courseName)
+    {
+        codeNumber = courseCode;
+        title = courseName;
+    }
+
+    
+}
