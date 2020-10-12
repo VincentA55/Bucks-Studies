81ee0f3f83518a729c93be95be7f91b5e00b67da
Vincent A <71930091+VincentA55@users.noreply.github.com>
2020-10-12 11:55:28 +0100

    Geting Started

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author Vincent A.
+ * @version 11/10/2020
+ */
+public class Ticket
+{
+    // the destination of the ticket
+    private String destination;
+    // the cost of the ticket
+    private int cost;
+    // the date purchased
+    private int datePurchased;
+
+   
+    private Ticket ()
+    {
+        cost = cost;
+        destination = "Aylesbury";
+        datePurchased = 2020;
+       
+    }
+    
+}
--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  * 
- * Modified by Student Name
+ * Modified by Vincent Assolutissimamente
  */
 public class TicketMachine
 {
