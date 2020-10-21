8fb03ec9a2952bee96553eb1dd3effe76806d0e2
Vincent A <71930091+VincentA55@users.noreply.github.com>
2020-10-21 12:31:19 +0100

    starting out

---
+++ app03-studentGrades/Course.java
@@ -0,0 +1,14 @@
+
+/**
+ * A class for the Course with its modules
+ *
+ * @author Vincent Assolutissimamente
+ * @version 2020.21.10
+ */
+public class Course
+{
+    //the name of the course
+    private String title;
+    // 
+    
+}
--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -4,8 +4,8 @@ import java.util.*;
  * The Student class represents a student in a student administration system.
  * It holds the student details relevant in our context.
  * 
- * @author Michael Kölling and David Barnes
- * @version 2016.02.29
+ * @author Vincent Assolutissimamente
+ * @version 2020.21.10
  */
 public class Student
 {
@@ -15,6 +15,8 @@ public class Student
     private String id;
     // the amount of credits for study taken so far
     private int credits;
+    //the course the student is on
+    private Course course;
     
     /**
      * Create a new student with a given name and ID number.
